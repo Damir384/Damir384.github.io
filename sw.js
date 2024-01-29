5f461d0ebbafd5c35385b0333e1f5355574c6b01
Damir384 <57034472+Damir384@users.noreply.github.com>
2024-01-30 02:26:47 +0700

    523

--- sw.js
+++
@@ -1,2 +0,0 @@
-'use strict';
-importScripts('sw-toolbox.js'); toolbox.precache(['index.php']); toolbox.router.get('img/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
