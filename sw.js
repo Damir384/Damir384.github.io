25bbea136e3225e47026ed153fed7b8baba111e9
Damir384 <57034472+Damir384@users.noreply.github.com>
2024-01-30 02:17:03 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,2 @@
+'use strict';
+importScripts('sw-toolbox.js'); toolbox.precache(['index.php']); toolbox.router.get('img/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
